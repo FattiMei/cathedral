595625268fb86ea42ce6f0a04edd19b78c0c6680
FattiMei <teomei68@gmail.com>
2024-07-04 14:11:25 +0200

    Add vectorized version of delay function

--- correlation.py
+++ correlation.py
@@ -24,3 +24,26 @@ def delay(track, time: int, level=0.5, feedback=0.5):
         buffer[i % time] = track[i] + feedback * buffer[i % time]
 
     return result
+
+
+def delay_vectorized(track, time: int, level=0.5, feedback=0.5):
+    if time == 0:
+        return track
+    elif time < 0 or time >= track.size:
+        return None
+
+    buffer = np.zeros(time)
+    result = np.zeros(track.size)
+
+    # update chunks of track
+    for i in range(track.size // time):
+        result[i*time : (i+1)*time] += track[i*time : (i+1)*time] + level * buffer
+        buffer = track[i*time : (i+1)*time] + feedback * buffer
+
+    # deal with remaining chunk
+    remainder = track.size % buffer.size
+
+    if remainder != 0:
+        result[(track.size - remainder) :] = track[(track.size - remainder) :] + level * buffer
+
+    return result
